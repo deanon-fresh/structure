f7a7b5878fdf72f0e2cce8c41ee12685e1cf91ba
deanon-fresh <deanon.fresh@yandex.ru>
2025-06-25 07:38:48 +0300

    Book.java

--- Book.java
+++ Book.java
@@ -1,49 +1,28 @@
 public class Book {
     public String title;
+    public Author author;
     public int releaseYear;
-    //public String author; // под редактирование
-    public Author surname;
-    public Author name;
-    public Author rating;
     public int pages;
 
-    public Book (String title,int releaseYear, /*String author, */ int pages) {
+    public Book (Author author, String title, int releaseYear, int pages) {
+        this.author = author;
         this.title = title;
         this.releaseYear = releaseYear;
-        //this.author = author;
         this.pages = pages;
+
     }
 
-
     public boolean isBig(){
-        if (pages > 500 ) {
-            return true;
-        } else {
-            return false;
-        }
+        return pages > 500;
         }
 
     public int estimatePrice(){
-        int y = (int) Math.floor(Math.sqrt(rating));
-        int x = (int) ((pages * 3) * y);
-        if (x > 250) {
-            return x;
-        } else {
-            x = 250;
-            return x;
-        }
+        int price = (int)(3 * pages * Math.sqrt(author.rating));
+        return Math.max(250, price);
     }
 
     public boolean matches(String word) {
-        if (title.contains(word)) {
-            return true;
-        } else if (surname.surname.contains(word)) { // почему дважды?
-            return true;
-        } else if (name.name.contains(word)) {
-            return true;
-        } else {
-            return false;
-        }
+        return title.contains(word) || author.name.contains(word) || author.surname.contains(word);
     }
 }
 
